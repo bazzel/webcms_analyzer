4922c6a844c7c21a7bd097de40eaecab4a583b27
Patrick Baselier <patrick@kabisa.nl>
2012-12-14 21:59:32 +0100

    Create WebCms::Chart::Data

--- lib/web_cms/chart/data.rb
+++ lib/web_cms/chart/data.rb
@@ -1,8 +1,47 @@
 module WebCms
   module Chart
     class Data
-      def initialize(web_cms_data)
+      attr_reader :web_cms_data
 
+      def initialize(web_cms_data)
+        @web_cms_data = web_cms_data
+      end
+
+      def title
+        'Request Time'
+      end
+
+      # @return [Hash] The hash's value is an Array with grouped frequencies
+      # #=> { 'webcms' => [1, 2, 1] }
+      def data
+        { 'webcms' => categorized_array.map { |e| e.last } }
+      end
+
+      # @return [Array<String>] Elements will be used as labels
+      # on the x-axis in the returned order
+      # #=> ['0-99', '100-199', ...]
+      def labels
+        categorized_array.map do |e|
+          k = e.first
+          "#{k}-#{k+99}"
+        end
+      end
+
+      private
+      def categorized_array
+        hash = Hash.new(0)
+        @web_cms_data.each do |e|
+          key = rounded(e)
+          hash[key] += 1
+        end
+        hash.sort
+      end
+
+      # Rounds last element of array down to nearest 100
+      # >> rounded([123, 456])
+      # # => 400
+      def rounded(e)
+        e.last / 100 * 100
       end
     end
   end
