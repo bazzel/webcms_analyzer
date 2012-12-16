8fe10e05a9261b531201e14066751096983873f2
Patrick Baselier <patrick@kabisa.nl>
2012-12-16 10:43:04 +0100

    Refactor Builder

--- lib/web_cms/builder.rb
+++ lib/web_cms/builder.rb
@@ -1,13 +1,11 @@
 require File.join(File.dirname(__FILE__), './data.rb')
-require File.join(File.dirname(__FILE__), './chart/data/base.rb')
-require File.join(File.dirname(__FILE__), './chart/data/request_time.rb')
-require File.join(File.dirname(__FILE__), './chart/data/total_request_time.rb')
-require File.join(File.dirname(__FILE__), './chart/data/expected_vs_retrieved.rb')
+Dir[File.join(File.dirname(__FILE__), 'chart', 'data', '*.rb')].each {|file| require file }
 require File.join(File.dirname(__FILE__), './chart/generator.rb')
 
 module WebCms
   class Builder
     attr_accessor :filename
+    attr_accessor :data
 
     class << self
       def process(filename)
@@ -21,15 +19,24 @@ module WebCms
     end
 
     def process
-      data = Data.new(@filename)
-      # From here, list all the flavours
-      # you'd like:
-      chart_data = Chart::Data::RequestTime.new(data)
+      process_chart 'RequestTime'
+      process_chart 'ExpectedVsRetrieved'
+      process_chart 'TotalRequestTime'
+    end
+
+    def data
+      @data ||= Data.new(@filename)
+    end
+
+    private
+    def process_chart(chart_type)
+      chart_data = chart_data(chart_type)
       Chart::Generator.generate(chart_data)
-      chart_data = Chart::Data::ExpectedVsRetrieved.new(data)
-      Chart::Generator.generate(chart_data)
-      chart_data = Chart::Data::TotalRequestTime.new(data)
-      Chart::Generator.generate(chart_data)
+    end
+
+    # Suppose I should create a Factory class for this...
+    def chart_data(chart_type)
+      Chart::Data.const_get(chart_type).new(data)
     end
   end
 end
